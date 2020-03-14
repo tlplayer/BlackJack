0e2932dd8a64c3649531e8b8a5dd9c7359ffa3b2
Tim <timothy.player@comcast.net>
2020-03-14 00:30:21 -0400

    Got the number of players read.

--- BlackJack.py
+++ BlackJack.py
@@ -2,7 +2,6 @@
 #Final Project sub project for 
 #demo game.
 
-
 #Take the number of players instantiate the array of cards 
 #and supply 
 def main():
@@ -11,6 +10,9 @@ def main():
 	cards = ['A',2,3,4,5,6,7,8,9,10,'J','Q','K']
 	deck = [0]*52
 
-	#Get user input of how many players.
-	num_players = input("Enter number of players:\n")
-	
+	#Get user input of how many players. and give everyone $50
+	num_players = int(input("Enter number of players:\n"))	
+	score = [50]*num_players
+
+#execute main()
+main()	
