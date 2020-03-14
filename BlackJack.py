d445b70cc4670511b964e93ed9239ce5b73d5124
Tim <timothy.player@comcast.net>
2020-03-14 00:12:09 -0400

    Starting the Blackjack Game

---
+++ BlackJack.py
@@ -0,0 +1,16 @@
+#Timothy Player
+#Final Project sub project for 
+#demo game.
+
+
+#Take the number of players instantiate the array of cards 
+#and supply 
+def main():
+	#Create a deck of 52 cards, the cards are one of the thirteen
+	#types.
+	cards = ['A',2,3,4,5,6,7,8,9,10,'J','Q','K']
+	deck = [0]*52
+
+	#Get user input of how many players.
+	num_players = input("Enter number of players:\n")
+	
