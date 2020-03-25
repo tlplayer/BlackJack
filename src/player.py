9d2407a46920084c14efa0e626f8e3b87dbfe0fd
Tim <timothy.player@comcast.net>
2020-03-25 18:22:59 -0500

    Reading up on classes still working on some stuff.

--- src/player.py
+++ src/player.py
@@ -12,18 +12,27 @@ class Player:
 		cards = []
 		self.deck = deck
 		standing = 0
-		standing = 0
+		bet = 0
 	def deal():
 		bust = 0
 		cards.append(deck.pop())
 		cards.append(deck.pop())
+	#
 	def hit():
 		if standing <= 21 and deck.size() != 0:
 			cards.append(deck.pop())
 			standing = self.sum()
+	#Prints the Player's cards and earnings and bet.
 	def print():
 		for i in range(cards):
 			print()
+	#Helper function to get total of player's
+	#cash
 	def sum():
 		for i in range(cards):
-			sum = 
+			sum += cards[i] 
+	#If the player busts remove their bet from their
+	#account.
+	def bust(bet):
+		standing = 0
+		money -= bet
